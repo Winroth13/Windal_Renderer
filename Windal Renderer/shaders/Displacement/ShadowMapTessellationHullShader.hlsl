afa7424af9aa865a8351274f71a9b4793ed53ca8
Johannled <johannes.ledendal@icloud.com>
2026-05-07 08:52:51 +0200

    Added Bricks Displacement Map

--- Windal Renderer/shaders/Displacement/ShadowMapTessellationHullShader.hlsl
+++ Windal Renderer/shaders/Displacement/ShadowMapTessellationHullShader.hlsl
@@ -38,7 +38,7 @@ PatchConstantOutput CalcHSPatchConstants(InputPatch<VertexShaderOutput, NUM_CONT
 {
     PatchConstantOutput output;
 
-    const float MAX_TESS_FACTOR = 30.0f;
+    const float MAX_TESS_FACTOR = 300.0f;
     const float MIN_TESS_FACTOR = 1.0f;
     const float MIN_TESS_DISTANCE = 20.0f;
     
