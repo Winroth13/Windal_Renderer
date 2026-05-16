4ae9796d2f20347c0501badb08b360d3d0b0d1f8
Winroth13 <emil.winroth@gmail.com>
2026-05-16 12:14:18 +0200

    Defaults To Using Dedicated GPU

--- Windal Renderer/src/main.cpp
+++ Windal Renderer/src/main.cpp
@@ -31,6 +31,11 @@
 
 #include "core/quadtree.h"
 
+extern "C" {
+	__declspec(dllexport) DWORD NvOptimusEnablement = 1;
+	__declspec(dllexport) DWORD AmdPowerXpressRequestHighPerformance = 1;
+}
+
 class TestApp : public App
 {
 public:
